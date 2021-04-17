b3b91c97410276c8d2b892876bc33b7ec4e69257
Banana.Hu <15129813553@163.com>
2021-04-17 11:29:51 +0800

    数据展示页面框架

--- lib/view/home_page/dataDisplay/humidity_list_page.dart
+++ lib/view/home_page/dataDisplay/humidity_list_page.dart
@@ -1,4 +1,4 @@
-import 'package:earthsite/utils/chart_widget.dart';
+import 'package:earthsite/widgets/chart_widget.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'dart:math';
